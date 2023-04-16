f4c2868f7fb79ccc2db853586ea9e6621a219889
sbansal1999 <sbansal1999@gmail.com>
2023-04-16 22:11:45 +0530

    Formatted "App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,56 @@
 /*
  * Temporary problems array schema
  */
-const problems = [{
+const problems = [
+  {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
-    acceptance: "42%"
-},{
+    acceptance: "42%",
+  },
+  {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
-    acceptance: "412%"
-},
-    {
-        title: "202. Happy Number",
-        difficulty: "Easy",
-        acceptance: "54.9%"
-    },
-    {
-        title: "203. Remove Linked List Elements",
-        difficulty: "Hard",
-        acceptance: "42%"
-    }];
-
+    acceptance: "412%",
+  },
+  {
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+  },
+  {
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+  },
+];
 
 function App() {
-
-    /* Add routing here, routes look like -
+  /* Add routing here, routes look like -
        /login - Login page
        /signup - Signup page
        /problemset/all/ - All problems (see problems array above)
        /problems/:problem_slug - A single problem page
-     */
+    */
 
-    return (
+  return (
     <div>
-        Finish the assignment! Look at the comments in App.jsx as a starting point
+      Finish the assignment! Look at the comments in App.jsx as a starting point
     </div>
-  )
+  );
 }
 
 // A demo component
 function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
+  const title = props.title;
+  const acceptance = props.acceptance;
+  const difficulty = props.difficulty;
 
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{acceptance}</td>
+      <td>{difficulty}</td>
     </tr>
+  );
 }
-export default App
+export default App;
