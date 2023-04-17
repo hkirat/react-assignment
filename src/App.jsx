dc12decae26f190d60a3a5b50b9a11a2060d3ec9
Naveen0910 <naveen850850@gmail.com>
2023-04-17 13:42:05 +0530

    Created req Comps getting started with LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
 /*
  * Temporary problems array schema
  */
@@ -22,38 +24,8 @@ const problems = [{
     }];
 
 
-function App() {
+const App = () => {
 
-    /* Add routing here, routes look like -
-       /login - Login page
-       /signup - Signup page
-       /problemset/all/ - All problems (see problems array above)
-       /problems/:problem_slug - A single problem page
-     */
-
-    return (
-    <div>
-        Finish the assignment! Look at the comments in App.jsx as a starting point
-    </div>
-  )
 }
 
-// A demo component
-function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
-
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
-    </tr>
-}
 export default App
