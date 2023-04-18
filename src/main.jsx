bcf073c36e598031d5db0e782d5839ec43e47a42
Mk4Levi <120794789+Mk4Levi@users.noreply.github.com>
2023-04-18 15:24:32 +0530

    Update App.jsx and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+//
+// Challenge :-
+// Add routing here, routes should look like -
+//   /signup - Signup page
+//   /login - Login page
+//   /problemset/all/ - All problems (see problems array above)
+//   /problems/:problem_slug - A single problem page
