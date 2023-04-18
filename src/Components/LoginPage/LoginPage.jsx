4bd186e71a689fc476a1f4c9ffa099c410a87d00
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-04-18 23:58:48 +0530

    feat: stylefix login singup

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <div
       id="login"
       className="h-[90vh] bg-[#ECEFF1] flex justify-center items-center"
     >
-      <form className="bg-white " method="post">
-        <div className="subform">
-          <label htmlFor="name">Name: </label>
-          <input type="text" name="name" placeholder="Your Name" />
-        </div>
+      <form
+        className="bg-white p-10 text-center flex flex-col lg:w-1/4 w:3/4 md:2/4 rounded-sm gap-6 "
+        method="post"
+      >
+        <img
+          src="https://assets.leetcode.com/static_assets/public/webpack_bundles/images/logo.c36eaf5e6.svg"
+          alt="leetcode-logo"
+          className="h-24 my-5"
+        />
 
-        <div className="subform">
-          <label htmlFor="email">Email: </label>
-          <input type="text" name="email" placeholder="Your Email" />
-        </div>
+        <input
+          value={userInfo.email}
+          className="border border-gray p-4 rounded focus:border focus:border-yellow-200"
+          type="text"
+          placeholder="Username or E-mail"
+        />
 
-        <div className="subform">
-          <label htmlFor="password">Password: </label>
-          <input type="text" name="password" placeholder="Your Password" />
-        </div>
+        <input
+          className="border border-gray p-4 rounded"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <button className="rounded bg-gradient-to-r from-[#546B7A] to-[#37474f] text-white p-4 text-xl">
+          Sign In
+        </button>
       </form>
     </div>
   );
