022455713a13f4f5872f304dc81aae45aa8ccdc2
sameershaik-coder <sameershaik.coder@gmail.com>
2023-04-18 09:52:45 +0530

    added logout to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,15 +4,30 @@ import AuthContext from './AuthContext';
 
 function Home() {
   const navigate = useNavigate();
-  const { isLoggedIn } = useContext(AuthContext);
-  console.log("isLoggedI : " + isLoggedIn)
+  const { isLoggedIn, setAuthState } = useContext(AuthContext);
+
   useEffect(() => {
     if (!isLoggedIn) {
       navigate('/login');
     }
   }, [isLoggedIn, navigate]);
 
-  return isLoggedIn ? <div>Welcome to the home page!</div> : null;
+  const handleLogout = () => {
+    // Perform logout logic here (e.g. make API call to backend)
+    // ...
+    // Update auth state
+    setAuthState({ isLoggedIn: false, token: null });
+    console.log("User log out successful")
+    // Navigate to login page
+    navigate('/login');
+  };
+
+  return isLoggedIn ? (
+    <div>
+      <div>Welcome to the home page!</div>
+      <button onClick={handleLogout}>Log Out</button>
+    </div>
+  ) : null;
 }
 
 export default Home;
