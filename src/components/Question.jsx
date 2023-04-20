2a5b011053018ecd252fe42cfee3bcd05678333f
sameershaik-coder <sameershaik.coder@gmail.com>
2023-04-20 19:30:06 +0530

    questions details completed

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,21 +2,25 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../AuthContext';
 import axios from 'axios';
+import config from '../config';
+import { useParams } from 'react-router-dom';
 
 function Question() {
   const navigate = useNavigate();
   const { isLoggedIn, token } = useContext(AuthContext);
   const [question, setQuestion] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    if (!isLoggedIn) {                                                                                          
       navigate('/login');
     } else {
-      const config = {
+      const reqconfig = {
         headers: { Authorization: `Bearer ${token}` },
-      };
+      };                                                                                        
+      const url = config.QUESTIONS_API_URL +"/"+ `${id}`
       axios
-        .get('http://localhost:3001/questions/1', config)
+        .get(url, reqconfig)
         .then((response) => {
           setQuestion(response.data);
         })
