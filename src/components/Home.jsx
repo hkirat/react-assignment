4d3761d2c335f5c43c05ec759693416d2d6f678b
sameershaik-coder <sameershaik.coder@gmail.com>
2023-04-20 15:53:33 +0530

    home page design complted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,9 +15,45 @@ function Home() {
   
 
   return isLoggedIn ? (
-    <div>
-      <div>Welcome to the new home page!</div>
-      
+    <div className='container'>
+        <div className='container mt-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-10'>
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th className="col">#</th>
+                            <th className="col">Title</th>
+                            <th className="col">Description</th>
+                            <th className="col">Solution</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   ) : null;
 }
