b4b1593064fc0031dbf73334efa9991fc62e08d4
Aryan-Singla <aryan.coding01@gmail.com>
2023-04-23 17:34:36 +0530

    A quick Save

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,78 @@
+import Logo from './../assets/LeetCode.jpg';
+import { useState } from "react";
+import SignIn from "./SignIn.jsx";
+import SignUp from "./SignUp.jsx";
+import Problems from "./Problems.jsx";
+
+const NavBar = () => {
+    const [activeLink, setActiveLink] = useState("");
+    const [showSignIn, setShowSignIn] = useState(false);
+    const [showSignUp, setShowSignUp] = useState(false);
+    // const [showProblems, setProblems] = useState(false)
+
+    const handleLinkClick = (event) => {
+        setActiveLink(event.target.text);
+    };
+    // const handleProblemsClick = (event) => {
+    //     setActiveLink(event.target.text);
+    //     setProblems(true);
+    //     setShowSignUp(false);
+    //     setShowSignIn(false);
+    // };
+    const handleSignInClick = () => {
+        setShowSignIn(true);
+        setShowSignUp(false);
+    };
+    const handleSignUpClick = () => {
+        setShowSignUp(true);
+        setShowSignIn(false);
+    };
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={Logo} height="24" width="100" className="d-inline-block align-text-top" alt="LeetCode Logo" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className={`nav-link ${activeLink === "Explore" ? "active" : ""}`} href="#" onClick={handleLinkClick}>Explore</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className={`nav-link ${activeLink === "Problems" ? "active" : ""}`} href="#" >Problems</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className={`nav-link ${activeLink === "Interview" ? "active" : ""}`} href="#" onClick={handleLinkClick}>Interview</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className={`nav-link ${activeLink === "Contest" ? "active" : ""}`} href="#" onClick={handleLinkClick}>Contest</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className={`nav-link ${activeLink === "Discuss" ? "active" : ""}`} href="#" onClick={handleLinkClick}>Discuss</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#"  onClick={handleSignUpClick}>Register</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" onClick={handleSignInClick}>Sign In</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {showSignIn && <SignIn/>}
+            {showSignUp && <SignUp/>}
+            {/*{showProblems && <Problems />}*/}
+        </>
+    )
+};
+
+export default NavBar;
---
+++ src/components/Problems.jsx
@@ -0,0 +1,65 @@
+const problems =
+    [
+        {
+            id:'201.',
+            title: " Bitwise AND of Numbers Range",
+            difficulty: "Medium",
+            acceptance: "42%"
+        },
+        {
+            id:'202.',
+            title: "Happy Number",
+            difficulty: "Easy",
+            acceptance: "54.9%"
+        },
+        {
+            id:'203.',
+            title: "Remove Linked List Elements",
+            difficulty: "Hard",
+            acceptance: "42%"
+        }
+    ];
+
+    function Problems(){
+        return <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Acceptance</th>
+                    <th scope="col">Difficulty</th>
+                </tr>
+            </thead>
+                {
+                    problems.map(problem => <ProblemStatement
+                        id={problem.id}
+                        title={problem.title}
+                        acceptance={problem.acceptance}
+                        difficulty={problem.difficulty}
+                    />)
+                }
+        </table>
+    }
+    function ProblemStatement(props) {
+        const id = props.id;
+        const title = props.title;
+        const acceptance = props.acceptance;
+        const difficulty = props.difficulty;
+        return (
+            <tr>
+                <td scope="row">
+                    {id}
+                </td>
+                <td>
+                    {title}
+                </td>
+                <td>
+                    {acceptance}
+                </td>
+                <td>
+                    {difficulty}
+                </td>
+            </tr>
+        )
+    }
+    export default Problems;
