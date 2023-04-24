9f09c48ad25aad2cce9a079c3fd1b1989a05fc8b
rinojames007 <rinojames.krl@gmail.com>
2023-04-24 23:40:14 +0530

    slight beautification of tables

--- src/pages/Problems.jsx
+++ src/pages/Problems.jsx
@@ -1,24 +1,24 @@
 // demo array
 const problems = [{
     id: 1,
-    title: "201. Bitwise AND of Numbers Range",
+    title: "Auto Complete Suggestions",
     difficulty: "Medium",
-    acceptance: "42%"
+    acceptance: "12%"
 },{
     id:2,
-    title: "201. Bitwise AND of Numbers Range",
+    title: "Bitwise AND of Numbers Range",
     difficulty: "Medium",
-    acceptance: "412%"
+    acceptance: "63%"
 },
     {
         id:3,
-        title: "202. Happy Number",
+        title: "Happy Number",
         difficulty: "Easy",
         acceptance: "54.9%"
     },
     {
         id:4,
-        title: "203. Remove Linked List Elements",
+        title: "Remove Linked List Elements",
         difficulty: "Hard",
         acceptance: "42%"
     }];
@@ -29,20 +29,21 @@ export default function Problems(props) {
     const title = props.title;
     const difficulty = props.difficulty;
     const acceptance = props.acceptance;
-
     return (
-        <table className={"w-full border-2"}>
+        <table className={"w-full items-center"}>
             <thead>
             <tr>
-                <th>id</th>
-                <th>title</th>
-                <th>difficulty</th>
-                <th>acceptance</th>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>DIFFICULTY</th>
+                <th>ACCEPTANCE</th>
             </tr>
             </thead>
             <tbody className={"text-center"}>
             {problems.map((problem) => (
-                <tr key={problem.id}>
+                <tr
+                    key={problem.id}
+                    className={problem.id % 2 === 0 ? "bg-slate-200 text-slate-800" : "bg-zinc-800 text-slate-300"}>
                     <td>{problem.id}</td>
                     <td><a href={"#"}>{problem.title}</a></td>
                     <td className={`problem-${problem.difficulty}`}>{problem.difficulty}</td>
