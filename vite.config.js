21f2237b81303eb7075363a3300c243537071ac6
Piyush <yadavpiyush222@gmail.com>
2023-04-26 22:46:36 +0530

    added build option in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 
-})
+});
