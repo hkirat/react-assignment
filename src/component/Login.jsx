06757887c67c4d32ad605d295852bd301d65d775
Salman Ansari <imsalman315@gmail.com>
2023-04-27 19:49:39 +0530

    Added register and login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,33 @@
 import './login.css'
 
 const Login = () => {
-  return <div className='login-container'>Login</div>
+  return (
+    <div className='login-container'>
+      <form className='login-form'>
+        <h2>Login</h2>
+        <div className='login-controller'>
+          <label className='login-label'>Email: </label>
+          <input
+            type='text'
+            className='login-input'
+            placeholder='Enter email...'
+          />
+        </div>
+        <div className='login-controller'>
+          <label className='login-label'>Password: </label>
+          <input
+            type='text'
+            className='login-input'
+            placeholder='Password...'
+          />
+        </div>
+        <div className='login-actions'>
+          <button className='login-btn-register'>Register</button>
+          <button className='login-btn-login'>Login</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Login
