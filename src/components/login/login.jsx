dd117010a19e8ed027521297b563f0d1d7dd7bcd
Aditya Dafe <97914006+KaKashi1210@users.noreply.github.com>
2023-04-27 21:04:42 +0530

    feat: better styles

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,13 +2,74 @@ import Navbar from "../navbar/Navbar"
 const Login = () => {
 return(
     <>
-        <Navbar/>
-	<div className="signup">
-	<span id="signupelement">email </span><input  placeholder="email"/> <br/> <br/>
-	<span id="signupelement">password</span> <input placeholder="password"/> <br/>
-	<br/>
-	<button id="signupbutton"type="submit">Login</button> 
-	</div>
+       	<Navbar/>
+<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 w-screen h-screen bg-teal-600 ">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Login to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-violet-600 "
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>
+              Login
+            </button>
+          </div>
+        </form>
+
+
+      </div>
+    </div>
     </>
 )
 }
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -12,34 +12,74 @@ const Signup = () => {
 	return (
 		<>
 		<Navbar/>
+<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
 
-		<form className="min-h-screen  flex flex-col justify-center py-12 px-6 lg:px-8">
-		<div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
 
-		<label for="email" className="block text-sm font-medium text-gray-700">Email</label>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-violet-600 "
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>
+              Sign in
+            </button>
+          </div>
+        </form>
 
 
-		<input 
-		className=" w-full px-3 py-2 rounded-lg  shadow-sm focus:outline-none focus:border-indigo-500
-		border-2 focus:ring-1 focus:ring-indigo-500 " placeholder="email" required/>
-
-
-		<br/>
-
-		<label for="email" className="block text-sm font-medium text-gray-700">Password</label>
-
-
-		<input type="password"	className=" w-full px-3 py-2 rounded-lg  shadow-sm focus:outline-none focus:border-indigo-500
-		border-2 focus:ring-1 focus:ring-indigo-500 " 
-		placeholder="password" required/>
-
-
-		<br/>
-		<button className=" px-10 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white
-		bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={handleclick}>Submit </button>
-		</div>
-		</form>
-		</>
+      </div>
+    </div>
+	</>
 	)
 
 }
