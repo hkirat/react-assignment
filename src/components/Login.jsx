7bd7f539a535093a061cb4d25efe6444eee2028a
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-05-14 11:43:54 +0530

    completed signup and login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-container">
+    <h2>Login</h2>
+      <form>
+        
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" className="form-control" id="username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
   )
 }
 
