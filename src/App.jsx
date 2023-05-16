8e4935840c2893290790c954e9b8b5be0d952bf2
MacMillon-VS <macsimson3@gmail.com>
2023-05-16 22:39:40 +0530

    commented a function

--- src/App.jsx
+++ src/App.jsx
@@ -109,21 +109,21 @@ function App() {
 }
 
 // A demo component
-function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
+// function ProblemStatement(props) {
+//     const title = props.title;
+//     const acceptance = props.acceptance;
+//     const difficulty = props.difficulty;
 
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
-    </tr>
-}
+//     return <tr>
+//         <td>
+//             {title}
+//         </td>
+//         <td>
+//             {acceptance}
+//         </td>
+//         <td>
+//             {difficulty}
+//         </td>
+//     </tr>
+// }
 export default App
