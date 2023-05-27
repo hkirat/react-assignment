7240ed998b46040021de2d9671067019b3e23434
Jassi <jaswinder.dandyan2000@gmail.com>
2023-05-27 15:18:58 +0530

    added navbar, login & signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,57 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+
 /*
  * Temporary problems array schema
  */
-const problems = [{
+const problems = [
+  {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
-    acceptance: "42%"
-},{
+    acceptance: "42%",
+  },
+  {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
-    acceptance: "412%"
-},
-    {
-        title: "202. Happy Number",
-        difficulty: "Easy",
-        acceptance: "54.9%"
-    },
-    {
-        title: "203. Remove Linked List Elements",
-        difficulty: "Hard",
-        acceptance: "42%"
-    }];
-
+    acceptance: "412%",
+  },
+  {
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+  },
+  {
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+  },
+];
 
 function App() {
-
-    /* Add routing here, routes look like -
+  /* Add routing here, routes look like -
        /login - Login page
        /signup - Signup page
        /problemset/all/ - All problems (see problems array above)
        /problems/:problem_slug - A single problem page
      */
-
-    return (
-    <div>
-        Finish the assignment! Look at the comments in App.jsx as a starting point
-    </div>
-  )
+  return (
+    <BrowserRouter>
+      <div className="bg-slate-100 h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-grow flex items-center justify-center">
+          <Routes>
+            <Route path="/" element={<Signup />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            {/*<Route path="/problemset/all" element={<ProblemSet />} />
+        <Route path="/problems/:problem_slug" element={<Problem />} /> */}
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-// A demo component
-function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
-
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
-    </tr>
-}
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
