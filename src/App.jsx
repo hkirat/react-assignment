2b8139fad85f3e2e563c942e737b0fa9432c6793
maheshpareek <pareekm644@gmail.com>
2023-05-29 16:04:35 +0530

    Initial project upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,127 @@
-/*
- * Temporary problems array schema
- */
-const problems = [{
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useParams } from 'react-router-dom';
+
+const problems = [
+  {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
     acceptance: "42%"
-},{
+  },
+  {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
     acceptance: "412%"
-},
-    {
-        title: "202. Happy Number",
-        difficulty: "Easy",
-        acceptance: "54.9%"
-    },
-    {
-        title: "203. Remove Linked List Elements",
-        difficulty: "Hard",
-        acceptance: "42%"
-    }];
-
+  },
+  {
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%"
+  },
+  {
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%"
+  }
+];
 
 function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/problemset/all/">All Problems</Link>
+            </li>
+          </ul>
+        </nav>
 
-    /* Add routing here, routes look like -
-       /login - Login page
-       /signup - Signup page
-       /problemset/all/ - All problems (see problems array above)
-       /problems/:problem_slug - A single problem page
-     */
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/problemset/all/" element={<ProblemSetPage />} />
+          <Route path="/problems/:problem_slug" element={<ProblemPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
-    return (
+function LoginPage() {
+  return (
     <div>
-        Finish the assignment! Look at the comments in App.jsx as a starting point
+      <h1>Login Page</h1>
     </div>
-  )
+  );
 }
 
-// A demo component
+function SignupPage() {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+    </div>
+  );
+}
+
+function ProblemSetPage() {
+  return (
+    <div>
+      <h1>All Problems</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Acceptance</th>
+            <th>Difficulty</th>
+          </tr>
+        </thead>
+        <tbody>
+          {problems.map((problem, index) => (
+            <ProblemStatement key={index} {...problem} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function ProblemPage() {
+  const { problem_slug } = useParams();
+
+  // Find the problem based on the slug
+  const problem = problems.find(
+    problem => problem.title.toLowerCase().replace(/\s/g, '-') === problem_slug
+  );
+
+  if (!problem) {
+    return <h2>Problem not found!</h2>;
+  }
+
+  return (
+    <div>
+      <h2>{problem.title}</h2>
+      <p>Difficulty: {problem.difficulty}</p>
+      <p>Acceptance: {problem.acceptance}</p>
+    </div>
+  );
+}
+
 function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
+  const { title, acceptance, difficulty } = props;
 
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{acceptance}</td>
+      <td>{difficulty}</td>
     </tr>
+  );
 }
-export default App
+
+export default App;
