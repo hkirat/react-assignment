9c3bf646f896d85c0e160731422d7e3e3946ee23
Tushar Jaiswal <tusharj045@gmail.com>
2023-06-01 02:18:03 +0530

    updated path \ to /

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className={styles.footerData}>
                 <div>Copyright © 2023.</div>
                 <div>Tushar Jaiswal</div>
-                <div><a href='https://www.linkedin.com/in/tushar-jaiswal-331202172/' target="_blank"><img className={styles.logo} src='public\logo\LI-In-Bug.png' alt='LinkedIn'/></a> <a href='https://github.com/Tusharj045' target="_blank"><img className={styles.logo} src='public\logo\github-mark-white.png' alt='Github'/></a></div>
+                <div><a href='https://www.linkedin.com/in/tushar-jaiswal-331202172/' target="_blank"><img className={styles.logo} src='public/logo/LI-In-Bug.png' alt='LinkedIn'/></a> <a href='https://github.com/Tusharj045' target="_blank"><img className={styles.logo} src='public/logo/github-mark-white.png' alt='Github'/></a></div>
             </div>
         </div>
     )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
         <div className={styles.navBar}>
             <div className={styles.section1}>
                 <div>
-                    <Link to={'/'}><img className={styles.userLogo} src="public\logo\coding.png" alt="Logo" /></Link>
+                    <Link to={'/'}><img className={styles.userLogo} src="public/logo/coding.png" alt="Logo" /></Link>
                 </div>
                 <div>
-                    <Link to={'/problemset/all'}><img className={styles.userLogo} src="public\logo\solution.png" alt="Problems" /></Link>
+                    <Link to={'/problemset/all'}><img className={styles.userLogo} src="public/logo/solution.png" alt="Problems" /></Link>
                 </div>
 
             </div>
             <div className={styles.section2}>
-                <Link to={'/login'}><img className={styles.userLogo} src="public\logo\user.png" alt="Login" /></Link>
+                <Link to={'/login'}><img className={styles.userLogo} src="public/logo/user.png" alt="Login" /></Link>
                 
             </div>
         </div>
