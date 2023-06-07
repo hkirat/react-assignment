16953cd713c55e82fbae67f4b41c16829a3ec30c
djay-S <dhananjaysinghkushwaha2@gmail.com>
2023-06-07 21:13:18 +0530

    Designed Login Page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,23 @@
 function Login() {
     return(
-        <div className='login'>
-            <input type = "text" placeholder="Email"/>
-            <input type = "text" placeholder="Password"/>
+        <div className='login-screen'>
+            <div className='login'>
+                <h2>F-leetcode</h2>
+                <div className="input-div">
+                    <div className="input-mini-dev">
+                        <input type = "text" placeholder="Email"/>
+                        <div className="input-validation-msg">Enter the correct email</div>
+                    </div>
+                    <div className="input-mini-div">
+                        <input type = "text" placeholder="Password"/>
+                        <div className= "input-validation-msg">Enter the correct password</div>
+                    </div>
+                    <div className="actions">
+                        <button className="signup-btn">Sign Up</button>
+                        <button className="login-btn">Login</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
