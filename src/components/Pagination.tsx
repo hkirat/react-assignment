a98536785ff0679532332848aed173b0da713db2
Varu98 <vardaan@wisdomtreeventures.com>
2023-06-15 16:49:37 +0530

    add : react assignment in ts and chakra ui

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,18 @@
+import { Box, HStack } from "@chakra-ui/react";
+
+const Pagination = () => {
+  return (
+    <HStack>
+      <Box>1</Box>
+      <Box>2</Box>
+      <Box>3</Box>
+      <Box>4</Box>
+      <Box>5</Box>
+      <Box>6</Box>
+      <Box>7</Box>
+      <Box>8</Box>
+    </HStack>
+  );
+};
+
+export default Pagination;
