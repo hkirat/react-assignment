81aeb061a3e052472c2cc59a6782088992788d4f
Akash Singh <developer.akash8@gmail.com>
2023-07-16 15:55:10 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,36 @@
-/*
- * Temporary problems array schema
- */
-const problems = [{
-    title: "201. Bitwise AND of Numbers Range",
-    difficulty: "Medium",
-    acceptance: "42%"
-},{
-    title: "201. Bitwise AND of Numbers Range",
-    difficulty: "Medium",
-    acceptance: "412%"
-},
-    {
-        title: "202. Happy Number",
-        difficulty: "Easy",
-        acceptance: "54.9%"
-    },
-    {
-        title: "203. Remove Linked List Elements",
-        difficulty: "Hard",
-        acceptance: "42%"
-    }];
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Problems from "./pages/Problems";
+import Problem from "./components/Problem";
 
 function App() {
-
-    /* Add routing here, routes look like -
+  /* Add routing here, routes look like -
        /login - Login page
        /signup - Signup page
-       /problemset/all/ - All problems (see problems array above)
-       /problems/:problem_slug - A single problem page
-     */
+       /problemset/all/ - All problems 
+       /problems/:problem_slug - A single problem page              */
 
-    return (
-    <div>
-        Finish the assignment! Look at the comments in App.jsx as a starting point
-    </div>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              Finish the assignment! Look at the comments in App.jsx as a
+              starting point
+            </div>
+          }
+        />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="problems/all" element={<Problems />} />
+        <Route path="problems/:id" element={<Problem />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-// A demo component
-function ProblemStatement(props) {
-    const title = props.title;
-    const acceptance = props.acceptance;
-    const difficulty = props.difficulty;
-
-    return <tr>
-        <td>
-            {title}
-        </td>
-        <td>
-            {acceptance}
-        </td>
-        <td>
-            {difficulty}
-        </td>
-    </tr>
-}
-export default App
+export default App;
