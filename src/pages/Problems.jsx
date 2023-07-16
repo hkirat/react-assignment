d59b210e12147f4687bae59806c78743f830b668
Akash Singh <developer.akash8@gmail.com>
2023-07-16 15:39:36 +0530

    Create pages

---
+++ src/pages/Problems.jsx
@@ -0,0 +1,46 @@
+/*
+ * Temporary problems array schema
+ */
+const problems = [
+  {
+    title: "201. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "42%",
+  },
+  {
+    title: "201. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "412%",
+  },
+  {
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%",
+  },
+  {
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%",
+  },
+];
+
+function Problems() {
+  return <div>Problems</div>;
+}
+
+// A demo component
+function ProblemStatement(props) {
+  const title = props.title;
+  const acceptance = props.acceptance;
+  const difficulty = props.difficulty;
+
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{acceptance}</td>
+      <td>{difficulty}</td>
+    </tr>
+  );
+}
+
+export default Problems;
