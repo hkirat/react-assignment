f9c56e8c7c2c34530e642d75eeb6bacd0346b671
Akash Singh <developer.akash8@gmail.com>
2023-07-16 23:39:50 +0530

    Implement UI for Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
 function Login() {
-  return <div>Login</div>;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const data = e.target.querySelectorAll("input");
+    
+    console.log("Email :", data[0].value);
+    console.log("Password :", data[1].value);
+  }
+
+  return (
+    <section>
+      <div className="login">
+        <h1>Login</h1>
+
+        <form onSubmit={handleLogin}>
+          <input type="email" placeholder="Enter email" />
+          <input type="password" placeholder="Enter password" />
+          <button type="submit">Login</button>
+        </form>
+
+        <p>Don't have account? <Link to="/signup"><span>Sign up</span></Link></p>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
