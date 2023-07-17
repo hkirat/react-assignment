494fbe98c0b1ab16acd67c1729cad5adbd345b7b
Akash Singh <developer.akash8@gmail.com>
2023-07-17 20:04:46 +0530

    SolveProblem - fetch data from the server

--- src/pages/SolveProblem.jsx
+++ src/pages/SolveProblem.jsx
@@ -1,4 +1,5 @@
-import { useParams, Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 // {
 //     "id": 1432,
@@ -13,33 +14,52 @@ import { useParams, Link } from "react-router-dom"
 //     ]
 //   },
 
-
 function SolveProblem() {
   const { id } = useParams();
+  const [problem, setProblem] = useState({});
+
+  useEffect(() => {
+    async function getProblem() {
+      const response = await fetch(
+        `http://127.0.0.1:3001/questions?problemId=${id}`
+      );
+      const problem = await response.json();
+
+      setProblem(problem.question);
+    }
+
+    getProblem();
+  }, []);
 
   return (
     <section className="SolveProblem">
-        <div className="problem__statement">
-            <Link to="/problems/all" className="goBack">{'<'} Problems</Link>
-            <div className="problem__head">
-                <p>1432. Maximum Element</p>
-                <p>Easy</p>
-            </div>
-            <p className="problem__description">Given an array, return the maximum of the array</p>
-            <div className="testcases">
-                <p>Testcases</p>
-                <div className="testcase">
-                    <p>Input: [1,2,3,4,5]</p>
-                    <p>Output: 5</p>
-                </div>
-            </div>
+      <div className="problem__statement">
+        <Link to="/problems/all" className="goBack">
+          {"<"} Problems
+        </Link>
+        <div className="problem__head">
+          <p>
+            {problem.id}. {problem.title}
+          </p>
+          <p>{problem.difficulty}</p>
         </div>
-        <div className="problem__input">
-            <textarea placeholder="Enter the code"></textarea>
-            <button>Submit</button>
+        <p className="problem__description">{problem.description}</p>
+        <div className="testcases">
+          <p>Testcases</p>
+          {problem.testCases?.map((testcase, i) => (
+            <div className="testcase" key={i}>
+              <p>Input: {testcase.input}</p>
+              <p>Output: {testcase.output}</p>
+            </div>
+          ))}
         </div>
+      </div>
+      <div className="problem__input">
+        <textarea placeholder="Enter the code"></textarea>
+        <button>Submit</button>
+      </div>
     </section>
-  )
+  );
 }
 
-export default SolveProblem
+export default SolveProblem;
