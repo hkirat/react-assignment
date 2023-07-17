5db55ca55f74a603067e4b0d1613e034dad30446
Akash Singh <developer.akash8@gmail.com>
2023-07-17 12:31:39 +0530

    header - linked to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import "../App.css";
 import logo from "../assets/vite.svg";
 
 function Header() {
   return (
-    <nav>
-      <img src={logo} alt="logo" />
-      <h1>LiteCode</h1>
-    </nav>
+    <Link to="/" style={{color: "black", textDecoration: "none"}}>
+      <nav>
+        <img src={logo} alt="logo" />
+        <h1>LiteCode</h1>
+      </nav>
+    </Link>
   );
 }
 
