b748d0fa3f8c9bf1ffa3ae52d8e9130656835805
Akash Singh <developer.akash8@gmail.com>
2023-07-17 16:12:58 +0530

    login - connected to the backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,32 @@ import { Link } from "react-router-dom";
 
 function Login() {
 
+  async function login(email, password){
+    try {
+      const response = await fetch("http://127.0.0.1:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+      })
+
+      const json = await response.json();
+      localStorage.setItem("token", json.token);
+      alert(json.message)
+      
+    } catch (err){
+      console.log("Error :\n", err);
+    }
+  }
+
   const handleLogin = (e) => {
     e.preventDefault();
 
     const data = e.target.querySelectorAll("input");
     
+    login(data[0].value, data[1].value);
+
     console.log("Email :", data[0].value);
     console.log("Password :", data[1].value);
   }
@@ -17,8 +38,8 @@ function Login() {
         <h1>Login</h1>
 
         <form onSubmit={handleLogin}>
-          <input type="email" placeholder="Enter email" />
-          <input type="password" placeholder="Enter password" />
+          <input type="email" placeholder="Enter email" required/>
+          <input type="password" placeholder="Enter password" required/>
           <button type="submit">Login</button>
         </form>
 
